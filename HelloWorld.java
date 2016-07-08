0998c553179d837896a9ad0fe0d4f283324c8320
sankarselvaraj <sankar.selvaraj@gmail.com>
2016-07-08 14:47:56 -0400

    HelloWorld.java
    Initial file used for Git

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by SelvarajS on 7/8/2016.
+ */
+public class HelloWorld {
+    private void printMethod(String name){
+        System.out.println("Welcome "+name);
+
+    }
+    public static void main(String args[]){
+        HelloWorld hw = new HelloWorld();
+        hw.printMethod("Sankar");
+    }
+}
